1a67394168312a56cb2f81e6f2bf409b560e2e83
Kartik Keyan Kant <kartik.kant@nammayatri.in>
2024-12-27 15:41:30 +0530

    Removed android blur code

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import VibrancyViewUntyped from './components/VibrancyView';
 import type { View } from 'react-native'
 
 import type { BlurViewProps as BlurViewPropsIOS } from './components/BlurView.ios';
-import type { BlurViewProps as BlurViewPropsAndroid } from './components/BlurView.android';
 import type { VibrancyViewProps as VibrancyViewPropsIOS } from './components/VibrancyView.ios';
 
 type BlurViewProps = BlurViewPropsIOS | BlurViewPropsAndroid;
